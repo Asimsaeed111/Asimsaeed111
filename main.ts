16f9bf7d9e3b41cbd6dee83fab440b657a27f4d6
Asimsaeed111 <161206949+Asimsaeed111@users.noreply.github.com>
2024-03-04 15:16:28 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,6 @@
+let myname: string = 'asim saeed';
+console.log(myname)
+console.log(myname.charAt(0).toUpperCase() + myname.slice(1).toLowerCase());
+console.log("hello", myname, 'would you like to eat something?');
+console.log(myname.toUpperCase());
+console.log(myname.toLowerCase());
